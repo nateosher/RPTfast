3cea4ea061447e3087a16f73516865371319ca4e
Sophie Hoffman <shoffm@umich.edu>
2021-12-10 16:15:41 +0000

    adding vignette

--- devtools_init_script.R
+++ devtools_init_script.R
@@ -1,1 +1,8 @@
 #initializing rptfast package
+install.packages("devtools")
+library(devtools)
+#create package
+create_package("/Users/sophiehoffman/Desktop/ggf.fast")
+use_mit_license()
+use_package("regentrans")
+use_package("Rcpp")
